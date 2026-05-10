ef771125cd587485fb88f11c9879ac1f37798b0d
Hay Dick <hayd@Fox.co.il>
2026-05-10 13:14:05 +0300

    Limit workflow to FOX HOME login monitoring

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,14 +38,6 @@ export default defineConfig({
     },
 
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
 
     /* Test against mobile viewports. */
     // {
